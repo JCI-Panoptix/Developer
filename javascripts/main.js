53c19ba24c7bfe47a12babae64f8ae61420adda2
John Bernd <jbernd18@live.com>
2014-02-14 15:17:56 -0600

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,19 +1,2 @@
 console.log('This would be the main JS file.');
 
-document.write('<DIV ALIGN="LEFT">\ 
-<TABLE BORDER="1" CELLSPACING="2" CELLPADDING="2" STYLE="FONT-SIZE: 10pt; FONT-FAMILY: Verdana" BGCOLOR="#CCCCFF" BORDERCOLOR="#94b8e4" BORDERCOLORLIGHT="#000000" BORDERCOLORDARK="#FFFFFF">\ 
-<TR>\ 
-<TD ALIGN="CENTER"><A HREF="Home.html">Home</A></TD>\ 
-</TR><TR>\ 
-<TD ALIGN="CENTER"><A HREF="Vend.html">Vendors</A></TD>\ 
-</TR><TR>\ 
-<TD ALIGN="CENTER"><A HREF="Sites.html">Sites</A></TD>\ 
-</TR><TR>\ 
-<TD ALIGN="CENTER"><A HREF="Submit.html">Submittal</A></TD>\ 
-</TR><TR>\ 
-<TD ALIGN="CENTER"><A HREF="Jokes.html">Jokes</A></TD>\ 
-</TR><TR>\ 
-<TD ALIGN="CENTER"><A HREF="Contact.html">Contact</A></TD>\ 
-</TR>\ 
-</TABLE>\ 
-</DIV>');
